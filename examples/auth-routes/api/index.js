8e72fa899bd582bcda8c81d77371e6eba36c468e
Shumin Guo <gsmsteve@gmail.com>
2019-10-05 12:09:37 -0700

    Initial version
    Nuxt starter.

--- examples/auth-routes/api/index.js
+++ examples/auth-routes/api/index.js
@@ -1,36 +1,36 @@
-import express from 'express'
+import express from "express";
 
 // Create express router
-const router = express.Router()
+const router = express.Router();
 
 // Transform req & res to have the same API as express
 // So we can use res.status() & res.json()
-const app = express()
+const app = express();
 router.use((req, res, next) => {
-  Object.setPrototypeOf(req, app.request)
-  Object.setPrototypeOf(res, app.response)
-  req.res = res
-  res.req = req
-  next()
-})
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
 
 // Add POST - /api/login
-router.post('/login', (req, res) => {
-  if (req.body.username === 'demo' && req.body.password === 'demo') {
-    req.session.authUser = { username: 'demo' }
-    return res.json({ username: 'demo' })
+router.post("/login", (req, res) => {
+  if (req.body.username === "demo" && req.body.password === "demo") {
+    req.session.authUser = { username: "demo" };
+    return res.json({ username: "demo" });
   }
-  res.status(401).json({ message: 'Bad credentials' })
-})
+  res.status(401).json({ message: "Bad credentials" });
+});
 
 // Add POST - /api/logout
-router.post('/logout', (req, res) => {
-  delete req.session.authUser
-  res.json({ ok: true })
-})
+router.post("/logout", (req, res) => {
+  delete req.session.authUser;
+  res.json({ ok: true });
+});
 
 // Export the server middleware
 export default {
-  path: '/api',
+  path: "/api",
   handler: router
-}
+};
